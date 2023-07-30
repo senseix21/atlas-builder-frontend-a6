2caa53da5f9f809e3b56a0ad1bd39282799dde70
senseiX21 <mahdix2118@protonmail.com>
2023-07-30 23:39:12 +0100

    Updated footer comp.

--- src/components/ui/FooterComponent.jsx
+++ src/components/ui/FooterComponent.jsx
@@ -3,30 +3,59 @@ import Link from 'next/link';
 
 const { Footer } = Layout;
 
-const footerNavItems = [
-    { title: 'Home', key: 'home', link: '/' },
-    { title: 'Shop', key: 'shop', link: '/shop' },
-    { title: 'About', key: 'about', link: '/about' },
-    { title: 'Contact', key: 'contact', link: '/contact' },
-];
+
 
 const FooterComponent = () => {
     return (
-        <Footer style={{
-            textAlign: 'center',
-            display: "flex",
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            backgroundColor: 'white',
-        }}>
-            <Menu theme="light" mode="horizontal" >
-                {footerNavItems.map((item) => (
-                    <Menu.Item key={item.key}>
-                        <Link href={item.link}>{item.title}</Link>
-                    </Menu.Item>
-                ))}
+        <Footer className="text-center bg-black text-white md:flex md:justify-between md:items-center">
+            <Menu theme="dark" mode="vertical" className="mb-4 md:mb-0">
+                <Menu.Item>
+                    <Link href={'/'}>
+                        <p className="text-white">Home</p>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href={'/shop'}>
+                        <p className="text-white">Shop</p>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href={'/about'}>
+                        <p className="text-white">About</p>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href={'/contact'}>
+                        <p className="text-white">Contact</p>
+                    </Link>
+                </Menu.Item>
+
             </Menu>
-            Atlas Builder ©2023 Created by senseiX
+            <div className="text-gray-500 mx-5">
+                <h2 className="text-xl font-semibold mb-2">About Us</h2>
+                <p className="text-sm ">
+                    Atlas Builder is a leading online store for all your PC building needs.
+                    We offer a wide range of high-quality components, including CPUs,
+                    motherboards, RAM, power supply units, storage devices, monitors, and more.
+                    Our mission is to provide our customers with the best products and services,
+                    making it easy and enjoyable to build their dream PC.
+                </p>
+            </div>
+            <div className="text-gray-500 mb-4 md:mb-0">
+                <p>Subscribe to our newsletter:</p>
+                <input
+                    type="text"
+                    placeholder="Enter your email"
+                    className="bg-gray-300 px-2 py-1 rounded-md text-black"
+                />
+                <button className="bg-blue-600 text-white px-4 py-1 ml-2 rounded-md">
+                    Subscribe
+                </button>
+                <p className="text-gray-500 my-10">
+                    Atlas Builder ©2023 Created by senseiX
+                </p>
+            </div>
+
         </Footer>
     );
 };
