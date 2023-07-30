2beecfdaefe72a565f28d3f3a80a2dbe037d3a21
senseiX21 <mahdix2118@protonmail.com>
2023-07-30 23:49:26 +0100

    updated login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,48 +1,46 @@
 import { GoogleOutlined, GithubOutlined } from "@ant-design/icons";
 import Head from "next/head";
-import styles from "@/styles/Login.module.css";
-import { signIn, useSession } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { useForm } from "react-hook-form";
-import { useAuthState, useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from "@/firebase/firebase.auth";
 import RootLayout from "@/components/layouts/RootLayout";
-
 
 const Login = () => {
     const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
 
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit } = useForm();
 
     const onSubmit = (data) => {
-        createUserWithEmailAndPassword(data.email, data.password)
-    }
+        createUserWithEmailAndPassword(data.email, data.password);
+    };
     console.log(user);
 
-
     return (
-        <div>
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
             <Head>
                 <title>Next Login</title>
             </Head>
-            <div className={styles.form}>
-                <h3>LOGIN</h3>
-                <div className={styles.social_icons}>
-                    <GoogleOutlined />
-                    <GithubOutlined onClick={() => signIn('github')} />
+            <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow">
+                <h3 className="text-2xl font-bold mb-4">LOGIN</h3>
+                <div className="flex items-center mb-4">
+                    <GoogleOutlined className="text-3xl mr-4 cursor-pointer" />
+                    <GithubOutlined className="text-3xl cursor-pointer" onClick={() => signIn('github')} />
                 </div>
-                <hr />
-                <form >
-                    <label htmlFor="">Your Email</label>
-                    <input {...register("email", { required: true })} type="email" />
-                    <label htmlFor="">Your Password</label>
-                    <input {...register("password", { required: true })} type="password" />
-                    <button style={{ color: 'white', backgroundColor: 'chocolate', borderRadius: '5px', }} type="submit">Login</button>
+                <hr className="my-4" />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label className="block mb-2" htmlFor="email">Your Email</label>
+                    <input {...register("email", { required: true })} type="email" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" />
+                    <label className="block mt-4 mb-2" htmlFor="password">Your Password</label>
+                    <input {...register("password", { required: true })} type="password" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" />
+                    <button type="submit" className="w-full mt-6 py-2 text-white bg-orange-600 rounded-md focus:outline-none hover:bg-chocolate-dark">
+                        Login
+                    </button>
                 </form>
             </div>
         </div>
     );
 };
-
 
 export default Login;
 
@@ -52,4 +50,4 @@ Login.getLayout = function (page) {
             {page}
         </RootLayout>
     )
-}
+};
